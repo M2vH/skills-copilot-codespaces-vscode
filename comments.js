7f1db6e4177d4abdd4aecb7728e1ffa1e7eda2c3
Marco M. von Hagen <M2vH@users.noreply.github.com>
2023-07-25 09:02:19 +0000

    fix server error

--- comments.js
+++ comments.js
@@ -58,3 +58,4 @@ http.createServer(function(req, res) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(comments.getComment(q.query.name));
             return res.end();
+        }}}).listen(8080);
